1c5c1d8979062c85f21191f947eda5a1db0161b5
Spencer-Lake <spencer.m.lake11@gmail.com>
2021-09-23 17:13:01 -0600

    finished the tests

--- tictactoe.test.ts
+++ tictactoe.test.ts
@@ -18,3 +18,28 @@ test('I can start a game', async () => {
     await button.click();
     
 });
+
+test('Should place an X in the top right square', async () => {
+    await driver.sleep(1000)
+    await driver.findElement(By.id('cell-2')).click();
+    await driver.sleep(1000)
+});
+
+test('should see if the computer places an O in the topleft box', async () => {
+    expect(await driver.findElement(By.id('cell-0'))).toEqual('O')
+})
+
+/* Wasn't quite able to figure this out.*/
+test('Should place an X in the very middle square', async () => {
+    await driver.findElement(By.id('cell-4')).click();
+    await driver.sleep(1000)
+});
+
+
+/*This one is expected to fail, but I wasn't quite able to figure it out as well.
+I will look forward to our 1 on 1 and see if you can point me in the right direction */
+test('Should see if the final messages displays the proper winner', async () => {
+    await driver.findElement(By.id('cell-6')).click();
+    await driver.sleep(1000)
+    expect(await driver.findElement(By.id('title'))).toEqual('X lost')
+});
